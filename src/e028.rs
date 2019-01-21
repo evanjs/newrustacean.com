ef0537c6b9e046d6acc325e8cc0a87a218516841
Chris Krycho <hello@chriskrycho.com>
2019-01-20 21:06:13 -0700

    WIP stubs for e028

---
+++ src/e028.rs
@@ -0,0 +1,205 @@
+//! Meet My Associates
+//!
+//! - **Date:** February 4, 2019
+//! - **Subject:** Associated items: functions and methods, constants, types,
+//!   and (very briefly!) items.
+//! - [**download mp3**][mp3]
+//! - [**script**][script]
+//!
+//! [mp3]: https://www.podtrac.com/pts/redirect.mp3/f001.backblazeb2.com/file/newrustacean/e028.mp3
+//! [script]: https://newrustacean.com/show_notes/e027/struct.script
+//!
+//! <audio style="width: 100%" title="e028: Meet My Associates" controls preload=metadata src="https://www.podtrac.com/pts/redirect.mp3/f001.backblazeb2.com/file/newrustacean/e028.mp3">
+//!
+//! Show Notes
+//! ----------
+//!
+//! TODO: notes!
+//!
+//! Sponsors
+//! --------
+//!
+//! TODO: Manning!
+//!
+//! ### Patreon Sponsors
+//! 
+//! TODO: update!
+//!
+//! - Adam Green
+//! - Aleksey Pirogov
+//! - Alexander Payne
+//! - Alexander Kryvomaz
+//! - Andrew Thompson
+//! - [Anthony Deschamps]
+//! - Anthony Scotti
+//! - [Behnam Esfahbod]
+//! - Benjamin Wasty
+//! - Brandon 'Spanky' Mills
+//! - Brian Casiello
+//! - Brian Manning
+//! - [Brian McCallister]
+//! - [Bryan Stitt]
+//! - Bryce Johnston
+//! - Caryn Finkelman
+//! - Cass Costello
+//! - Chap Lovejoy
+//! - [Charlie Egan]
+//! - Chip
+//! - [Chris Palmer]
+//! - Daniel
+//! - Dan Abrams
+//! - Daniel Bross
+//! - [Daniel Collin]
+//! - [Daniel Mason]
+//! - David Hewson
+//! - [Derek Morr]
+//! - Doug Reeves
+//! - Eugene Bulkin
+//! - Fábio Botelho
+//! - [Gaveen Prabhasara]
+//! - [Graham Wihlidal]
+//! - [Henri Sivonen]
+//! - [Ian Jones]
+//! - "Jake ""ferris"" Taylor"
+//! - Jako Danar
+//! - James Cooper
+//! - James Hagans II
+//! - Jerome Froelich
+//! - [Joar Wandborg]: Joar Wandborg
+//! - [John Rudnick]
+//! - Jon
+//! - [Jonathan Knapp]
+//! - Jonathan Turner
+//! - Joseph Hain
+//! - Joseph Marhee
+//! - Justin Ossevoort
+//! - Kai Yao
+//! - Keith Gray
+//! - Kilian Rault
+//! - Lee Jenkins
+//! - Luca Schmid
+//! - [Luiz Irber]
+//! - Lukas Eller
+//! - [Martin Heuschober]
+//! - Masashi Fujita
+//! - Matt Rudder
+//! - Matthew Brenner
+//! - Matthias Ruszala
+//! - [Max Jacobson]
+//! - Max R.R. Collada
+//! - [Messense Lv]
+//! - Micael Bergeron
+//! - [Michael Mc Donnell]
+//! - Michael Sanders
+//! - [Nathan Sculli]
+//! - [Nick Coish]
+//! - Nick Gideo
+//! - [Nick Stevens]
+//! - [Nicolas Pochet]
+//! - Olaf Leidinger
+//! - Oliver Uvman
+//! - [Oluseyi Sonaiya]
+//! - Ovidiu Curcan
+//! - [Pascal]
+//! - [Patrick O'Doherty]
+//! - Paul Naranja
+//! - Paul Osborne
+//! - Peter Scholtens
+//! - Peter Tillemans
+//! - Ralph Giles
+//! - [Ramon Buckland]
+//! - Randy MacLeod
+//! - Raph Levien
+//! - Richard Dallaway
+//! - Rob Tsuk
+//! - Robert Chrzanowski
+//! - [Ryan Blecher]
+//! - [Ryan Osial]
+//! - Scott Moeller
+//! - [Sebastián Ramírez Magrí]
+//! - [Simon Dickson]
+//! - Simon G
+//! - [Steffen Loen Sunde]
+//! - Steve Jenson
+//! - Steven Knight
+//! - Steven Murawski
+//! - [Stuart Hinson]
+//! - Tim Brooks
+//! - Tim Süberkrüb
+//! - Tom Prince
+//! - Toolmaker's Guild
+//! - Ty Overby
+//! - Tyler Harper
+//! - Victor Kruger
+//! - Will Greenberg
+//! - [William Roe]
+//! - Zak van der Merwe
+//! - Zachary Snyder
+//! - Zaki
+//!
+//! [Anthony Deschamps]: https://github.com/adeschamps
+//! [Behnam Esfahbod]: https://github.com/behnam
+//! [Brian McCallister]: https://skife.org/
+//! [Bryan Stitt]: http://www.stitthappens.com/
+//! [Charlie Egan]: https://charlieegan3.com
+//! [Chris Palmer]: http://home.red-oxide.org/
+//! [Damien Stanton]: https://github.com/damienstanton
+//! [Daniel Collin]: https://twitter.com/daniel_collin
+//! [Daniel Mason]: https://github.com/gisleburt
+//! [Daniel P. Clark]: https://6ftdan.com/
+//! [David W. Allen]: http://GitHub.com/DataRiot
+//! [Derek Morr]: https://twitter.com/derekmorr
+//! [Gaveen Prabhasara]: https://twitter.com/gaveen
+//! [Graham Wihlidal]: https://wihlidal.com/
+//! [Henri Sivonen]: https://hsivonen.fi/
+//! [Ian Jones]: https://www.ianmjones.com/
+//! [Joar Wandborg]: http://github.com/joar
+//! [Jonathan Knapp]: https://www.coffeeandcode.com/
+//! [John Rudnick]: http://www.cindur.com/
+//! [Luiz Irber]: http://luizirber.org/
+//! [Martin Heuschober]: https://github.com/epsilonhalbe
+//! [Max Jacobson]: https://twitter.com/maxjacobson
+//! [Messense Lv]: https://github.com/messense
+//! [Michael Mc Donnell]: https://www.linkedin.com/in/michaelmcdonnell/
+//! [Nathan Sculli]: http://influential.co/
+//! [Nick Coish]: http://github.com/ncoish
+//! [Nick Stevens]: https://github.com/nastevens
+//! [Nicolas Pochet]: https://github.com/n-pochet
+//! [Oluseyi Sonaiya]: http://oluseyi.info/
+//! [Pascal]: https://pascalhertleif.de/
+//! [Patrick O'Doherty]: https://twitter.com/patrickod
+//! [Philipp Keller]: https://twitter.com/hansapla
+//! [Ramon Buckland]: http://www.inosion.com
+//! [Ryan Blecher]: http://notryanb.github.io/
+//! [Ryan Osial]: https://github.com/osialr
+//! [Sebastián Ramírez Magrí]: https://www.twitter.com/sebasmagri
+//! [Simon Dickson]: https://www.simonhdickson.com/
+//! [Steffen Loen Sunde]: https://www.ntnu.edu/employees/steffen.sunde
+//! [Stuart Hinson]: http://stuarth.github.io/
+//! [William Roe]: http://willroe.me
+//!
+//! (Thanks to the couple people donating who opted out of the reward tier, as
+//! well. You know who you are!)
+//!
+//! ### Become a sponsor
+//!
+//! - [Patreon](https://www.patreon.com/newrustacean)
+//! - [Venmo](https://venmo.com/chriskrycho)
+//! - [Dwolla](https://www.dwolla.com/hub/chriskrycho)
+//! - [Cash.me](https://cash.me/$chriskrycho)
+//! - [Flattr](https://flattr.com/profile/chriskrycho)
+//! - [PayPal.me](https://paypal.me/chriskrycho)
+//!
+//!
+//! Contact
+//! -------
+//!
+//! - New Rustacean:
+//!       + Twitter: [@newrustacean](https://www.twitter.com/newrustacean)
+//!       + Email: [hello@newrustacean.com](mailto:hello@newrustacean.com)
+//! - Chris Krycho
+//!       + GitHub: [chriskrycho](https://github.com/chriskrycho)
+//!       + Twitter: [@chriskrycho](https://www.twitter.com/chriskrycho)
+
+#[doc(include = "../docs/e028-script.md")]
+pub struct Script;
